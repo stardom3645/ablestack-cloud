stardom3645/ablestack-cloud

commit 62a6d3aeb880dd50ad59b9a4b86a1d04347d1619

Review 11752 applied

utils/test/com/cloud/utils/TernaryTest.java
@@ -1,0 +1,18 @@
+package com.cloud.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TernaryTest {
+    @Test
+    public void testEquals() {
+        Assert.assertEquals(new Ternary<String, String, String>("a", "b", "c"), new Ternary<String, String, String>(
+                "a", "b", "c"));
+        Assert.assertFalse(new Ternary<String, String, String>("a", "b", "c")
+                .equals(new Ternary<String, String, String>("a", "b", "d")));
+        Assert.assertFalse(new Ternary<String, String, String>("a", "b", "c").equals(""));
+        Assert.assertFalse(new Ternary<String, String, String>("a", "b", "c").equals(null));
+        Assert.assertFalse(new Ternary<String, String, String>("a", "b", "c")
+                .equals(new Pair<String, String>("a", "b")));
+    }
+}

utils/src/com/cloud/utils/Ternary.java
@@ -61,7 +61,7 @@ public class Ternary<T, U, V> {
     
     @Override
     public boolean equals(Object obj) {
-        if (!(obj instanceof Pair)) {
+        if (!(obj instanceof Ternary)) {
             return false;
         }
         Ternary<?, ?, ?> that = (Ternary<?, ?, ?>)obj;

commit de4ca5eea56ce06dc449a4ecf3e5353638a0c737

Fix RAT check broken by 62a6d3aeb880dd

utils/test/com/cloud/utils/TernaryTest.java
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package com.cloud.utils;
 
 import org.junit.Assert;