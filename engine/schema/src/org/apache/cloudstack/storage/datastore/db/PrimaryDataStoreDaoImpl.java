stardom3645/ablestack-cloud

commit 155db37c4b2fe514e2588858496aeb7f2cab7c9e

Fix findbugs SQL_PREPARED_STATEMENT_GENERATED_FROM_NONCONSTANT_STRING warning in PrimaryDataStoreDaoImpl.java
Remove trailing spaces that checkstyle complained about

Signed-off-by: Daan Hoogland <daan.hoogland@gmail.com>

This closes #474

engine/schema/src/org/apache/cloudstack/storage/datastore/db/PrimaryDataStoreDaoImpl.java
@@ -392,20 +392,21 @@ public class PrimaryDataStoreDaoImpl extends GenericDaoBase<StoragePoolVO, Long>
 
             StringBuilder sql = new StringBuilder(ZoneWideDetailsSqlPrefix);
 
-            for (Map.Entry<String, String> detail : details.entrySet()) {
-                sql.append("((storage_pool_details.name='")
-                    .append(detail.getKey())
-                    .append("') AND (storage_pool_details.value='")
-                    .append(detail.getValue())
-                    .append("')) OR ");
+            for (int i=0;i<details.size();i++){
+                sql.append("((storage_pool_details.name=?) AND (storage_pool_details.value=?)) OR ");
             }
             sql.delete(sql.length() - 4, sql.length());
             sql.append(ZoneWideDetailsSqlSuffix);
             TransactionLegacy txn = TransactionLegacy.currentTxn();
             try (PreparedStatement pstmt = txn.prepareStatement(sql.toString());){
+                int i=0;
+                for (Map.Entry<String, String> detail : details.entrySet()) {
+                    pstmt.setString(++i,detail.getKey());
+                    pstmt.setString(++i,detail.getValue());
+                }
                 List<StoragePoolVO> pools = new ArrayList<StoragePoolVO>();
                 if (pstmt != null) {
-                    int i = 1;
+                    i = 1;
                     pstmt.setLong(i++, dcId);
                     pstmt.setString(i++, ScopeType.ZONE.toString());
                     pstmt.setInt(i++, details.size());

commit f92391f3eb705050ceee6901c941558ee7cfecc9

Correcting an issue that was introduced with 155db37c4b2fe514e2588858496aeb7f2cab7c9e

engine/schema/src/org/apache/cloudstack/storage/datastore/db/PrimaryDataStoreDaoImpl.java
@@ -380,7 +380,6 @@ public class PrimaryDataStoreDaoImpl extends GenericDaoBase<StoragePoolVO, Long>
 
     @Override
     public List<StoragePoolVO> findZoneWideStoragePoolsByTags(long dcId, String[] tags) {
-        List<StoragePoolVO> storagePools = null;
         if (tags == null || tags.length == 0) {
             QueryBuilder<StoragePoolVO> sc = QueryBuilder.create(StoragePoolVO.class);
             sc.and(sc.entity().getDataCenterId(), Op.EQ, dcId);
@@ -399,17 +398,20 @@ public class PrimaryDataStoreDaoImpl extends GenericDaoBase<StoragePoolVO, Long>
             sql.append(ZoneWideDetailsSqlSuffix);
             TransactionLegacy txn = TransactionLegacy.currentTxn();
             try (PreparedStatement pstmt = txn.prepareStatement(sql.toString());){
-                int i=0;
-                for (Map.Entry<String, String> detail : details.entrySet()) {
-                    pstmt.setString(++i,detail.getKey());
-                    pstmt.setString(++i,detail.getValue());
-                }
                 List<StoragePoolVO> pools = new ArrayList<StoragePoolVO>();
                 if (pstmt != null) {
-                    i = 1;
+                    int i = 1;
+
                     pstmt.setLong(i++, dcId);
                     pstmt.setString(i++, ScopeType.ZONE.toString());
+
+                    for (Map.Entry<String, String> detail : details.entrySet()) {
+                        pstmt.setString(i++, detail.getKey());
+                        pstmt.setString(i++, detail.getValue());
+                    }
+
                     pstmt.setInt(i++, details.size());
+
                     try(ResultSet rs = pstmt.executeQuery();) {
                         while (rs.next()) {
                             pools.add(toEntityBean(rs, false));