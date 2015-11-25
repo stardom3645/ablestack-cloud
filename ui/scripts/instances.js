stardom3645/ablestack-cloud

commit 7b64c75663cda41505ca60a922acd6b3064276f5

CLOUDSTACK-9020: add ipaddress in instances view
Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

ui/scripts/instances.js
@@ -170,6 +170,9 @@
                     label: 'label.display.name',
                     truncate: true
                 },
+                ipaddress: {
+                    label: 'label.ip.address'
+                },
                 zonename: {
                     label: 'label.zone.name'
                 },
@@ -394,6 +397,11 @@
                     data: data,
                     success: function(json) {
                         var items = json.listvirtualmachinesresponse.virtualmachine;
+                        $.each(items, function(idx, vm) {
+                            if (vm.nic && vm.nic.length > 0 && vm.nic[0].ipaddress) {
+                                items[idx].ipaddress = vm.nic[0].ipaddress;
+                            }
+                        });
                         args.response.success({
                             data: items
                         });

commit 6d8b6ed3d5f73f12851a60362a3bfcc7dccd4a9b

ui: Fix undefined check in instances.js, regression from 7b64c75
Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

ui/scripts/instances.js
@@ -397,11 +397,13 @@
                     data: data,
                     success: function(json) {
                         var items = json.listvirtualmachinesresponse.virtualmachine;
-                        $.each(items, function(idx, vm) {
-                            if (vm.nic && vm.nic.length > 0 && vm.nic[0].ipaddress) {
-                                items[idx].ipaddress = vm.nic[0].ipaddress;
-                            }
-                        });
+                        if (items) {
+                            $.each(items, function(idx, vm) {
+                                if (vm.nic && vm.nic.length > 0 && vm.nic[0].ipaddress) {
+                                    items[idx].ipaddress = vm.nic[0].ipaddress;
+                                }
+                            });
+                        }
                         args.response.success({
                             data: items
                         });