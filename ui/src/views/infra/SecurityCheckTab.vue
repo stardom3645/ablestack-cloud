acbf7dca2c6adfde5e5e354ef16d26d451a81b27
Dajeong-Park <ekwjd7182@gmail.com>
2023-11-16 15:43:49 +0900

    테스트 로그 삭제

--- ui/src/views/infra/SecurityCheckTab.vue
+++ ui/src/views/infra/SecurityCheckTab.vue
@@ -363,20 +363,18 @@ export default {
       }
       this.deleteLoading = true
       api('deleteSecurityCheckResults', params).then(json => {
-        console.log(json)
-        // const jobId = json.deletesecuritycheckresultsresponse.jobid
-        // if (jobId) {
-        //   this.$pollJob({
-        //     jobId,
-        //     title: this.$t('label.action.delete.security.check.result'),
-        //     showLoading: !(this.selectedItems.length > 0 && this.showGroupActionModal),
-        //     loadingMessage: `${this.$t('label.deleting.security.check.results')} ${this.resource.name} ${this.$t('label.in.progress')}`,
-        //     catchMessage: this.$t('error.fetching.async.job.result'),
-        //     bulkAction: this.selectedItems.length > 0 && this.showGroupActionModal
-        //   })
-        // }
+        const jobId = json.deletesecuritycheckresultsresponse.jobid
+        if (jobId) {
+          this.$pollJob({
+            jobId,
+            title: this.$t('label.action.delete.security.check.result'),
+            showLoading: !(this.selectedItems.length > 0 && this.showGroupActionModal),
+            loadingMessage: `${this.$t('label.deleting.security.check.results')} ${this.resource.name} ${this.$t('label.in.progress')}`,
+            catchMessage: this.$t('error.fetching.async.job.result'),
+            bulkAction: this.selectedItems.length > 0 && this.showGroupActionModal
+          })
+        }
       }).catch(error => {
-        console.log(error)
         this.$notifyError(error)
       }).finally(() => {
         this.deleteLoading = false
