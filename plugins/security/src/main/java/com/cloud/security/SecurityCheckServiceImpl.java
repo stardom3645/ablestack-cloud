c6534e78b4b4e7b1d3a5b470bec9bf8a68dc8540
Dajeong-Park <ekwjd7182@gmail.com>
2023-11-27 14:33:55 +0900

    Update SecurityCheckServiceImpl.java

--- plugins/security/src/main/java/com/cloud/security/SecurityCheckServiceImpl.java
+++ plugins/security/src/main/java/com/cloud/security/SecurityCheckServiceImpl.java
@@ -284,7 +284,6 @@ public class SecurityCheckServiceImpl extends ManagerBase implements PluggableSe
                     EventTypes.EVENT_SECURITY_CHECK, "Failed to execute security check on the management server when operating the product ", new Long(0), null, 0);
                 alertManager.sendAlert(AlertManager.AlertType.ALERT_TYPE_MANAGMENT_NODE, 0, new Long(0), "Failed to execute security check on the management server when operating the product", "");
             }
-            
         }
         SecurityCheckVO connectivityVO = new SecurityCheckVO(msHostId, checkFinalResult, checkFailedList, type);
         connectivityVO.setMsHostId(msHostId);
