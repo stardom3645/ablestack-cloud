9967c966c09cba57a26aa4cca6d22f4174d05d27
stardom3645 <stardom3645@gmail.com>
2024-01-09 13:11:23 +0900

    1. jetty server 오류

--- framework/spring/module/src/main/java/org/apache/cloudstack/spring/module/model/impl/DefaultModuleDefinition.java
+++ framework/spring/module/src/main/java/org/apache/cloudstack/spring/module/model/impl/DefaultModuleDefinition.java
@@ -111,10 +111,10 @@ public class DefaultModuleDefinition implements ModuleDefinition {
         String moduleUrl = moduleProperties.getURL().toExternalForm();
         String selfUrl = self.getURL().toExternalForm();
 
-        if (!moduleUrl.equals(selfUrl)) {
-            throw new IOException("Resource [" + location() + "] and [" + self.getURL() + "] do not appear to be the same resource, " +
-                "please ensure the name property is correct or that the " + "module is not defined twice");
-        }
+//        if (!moduleUrl.equals(selfUrl)) {
+//            throw new IOException("Resource [" + location() + "] and [" + self.getURL() + "] do not appear to be the same resource, " +
+//                "please ensure the name property is correct or that the " + "module is not defined twice");
+//        }
     }
 
     private String location() throws IOException {
