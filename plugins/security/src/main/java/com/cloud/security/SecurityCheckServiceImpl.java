e38850a5e1adf06e1cf1dca49f0e035ef5465ca2
Dajeong-Park <ekwjd7182@gmail.com>
2024-04-16 15:44:33 +0900

    Update SecurityCheckServiceImpl.java

--- plugins/security/src/main/java/com/cloud/security/SecurityCheckServiceImpl.java
+++ plugins/security/src/main/java/com/cloud/security/SecurityCheckServiceImpl.java
@@ -254,7 +254,7 @@ public class SecurityCheckServiceImpl extends ManagerBase implements PluggableSe
             if ("Execution".equals(type)) {
                 ActionEventUtils.onCompletedActionEvent(CallContext.current().getCallingUserId(), CallContext.current().getCallingAccountId(), EventVO.LEVEL_ERROR,
                     EventTypes.EVENT_SECURITY_CHECK, "Failed to execute security check on the management server when running the product", new Long(0), null, 0);
-                    alertManager.sendAlert(AlertManager.AlertType.ALERT_TYPE_MANAGMENT_NODE, 0, new Long(0), "Failed to execute security check on the management server when running the product", "");
+                alertManager.sendAlert(AlertManager.AlertType.ALERT_TYPE_MANAGMENT_NODE, 0, new Long(0), "Failed to execute security check on the management server when running the product", "");
             } else if ("Routine".equals(type)) {
                 ActionEventUtils.onCompletedActionEvent(CallContext.current().getCallingUserId(), CallContext.current().getCallingAccountId(), EventVO.LEVEL_ERROR,
                     EventTypes.EVENT_SECURITY_CHECK, "Failed to execute security check schedule on the management server when operating the product", new Long(0), null, 0);
