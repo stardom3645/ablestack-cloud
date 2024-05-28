4e0de8c3def8a7996eb3a0eb9a3adc678fc7e68d
Abhishek Kumar <abhishek.mrt22@gmail.com>
2024-05-28 12:29:54 +0530

    ui: add move to top/bottom for acl rules list (#9119)
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/core/lazy_lib/icons_use.js
+++ ui/src/core/lazy_lib/icons_use.js
@@ -173,6 +173,8 @@ import {
   UserOutlined,
   UserSwitchOutlined,
   UploadOutlined,
+  VerticalAlignBottomOutlined,
+  VerticalAlignTopOutlined,
   WarningOutlined,
   WifiOutlined,
   ShopOutlined,
@@ -342,6 +344,8 @@ export default {
     app.component('UserOutlined', UserOutlined)
     app.component('UserSwitchOutlined', UserSwitchOutlined)
     app.component('UploadOutlined', UploadOutlined)
+    app.component('VerticalAlignBottomOutlined', VerticalAlignBottomOutlined)
+    app.component('VerticalAlignTopOutlined', VerticalAlignTopOutlined)
     app.component('WarningOutlined', WarningOutlined)
     app.component('WifiOutlined', WifiOutlined)
     app.component('ShopOutlined', ShopOutlined)
