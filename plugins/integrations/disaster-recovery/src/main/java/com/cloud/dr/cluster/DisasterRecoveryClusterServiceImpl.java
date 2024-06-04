f1142652435c2aae8eb4258f8e26fd2df0646018
Dajeong-Park <ekwjd7182@gmail.com>
2024-06-04 10:40:30 +0900

    Update DisasterRecoveryClusterServiceImpl.java

--- plugins/integrations/disaster-recovery/src/main/java/com/cloud/dr/cluster/DisasterRecoveryClusterServiceImpl.java
+++ plugins/integrations/disaster-recovery/src/main/java/com/cloud/dr/cluster/DisasterRecoveryClusterServiceImpl.java
@@ -653,7 +653,7 @@ public class DisasterRecoveryClusterServiceImpl extends ManagerBase implements D
                         if (drListResponse != null || !drListResponse.isEmpty()) {
                             for (GetDisasterRecoveryClusterListResponse dr : drListResponse) {
                                 if (dr.getName() == drCluster.getName()) {
-                                    string primaryDrId = dr.getId();
+                                    String primaryDrId = dr.getId();
                                     // secondary cluster db 업데이트
                                     secCommand = "deleteDisasterRecoveryCluster";
                                     secMethod = "GET";
