apache/cloudstack

server,cks: check if vm is cks node during vm destroy (#9057)
Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

utils/src/main/java/com/cloud/utils/component/ComponentContext.java
@@ -29,6 +29,7 @@ import javax.management.MalformedObjectNameException;
 import javax.management.NotCompliantMBeanException;
 import javax.naming.ConfigurationException;
 
+import org.apache.commons.collections4.MapUtils;
 import org.apache.log4j.Logger;
 import org.springframework.aop.framework.Advised;
 import org.springframework.beans.BeansException;
@@ -285,5 +286,23 @@ public class ComponentContext implements ApplicationContextAware {
 
     private static synchronized void initInitializeBeans(boolean initializeBeans) {
         s_initializeBeans = initializeBeans;
+    }
+
+    public static <T> T getDelegateComponentOfType(Class<T> beanType) {
+        if (s_appContextDelegates == null) {
+            throw new NoSuchBeanDefinitionException(beanType.getName());
+        }
+        T bean = null;
+        for (ApplicationContext context : s_appContextDelegates.values()) {
+            Map<String, T> map = context.getBeansOfType(beanType);
+            if (MapUtils.isNotEmpty(map)) {
+                bean = (T)map.values().toArray()[0];
+                break;
+            }
+        }
+        if (bean == null) {
+            throw new NoSuchBeanDefinitionException(beanType.getName());
+        }
+        return bean;
     }
 }

stardom3645/ablestack-cloud

utils/src/main/java/com/cloud/utils/component/ComponentContext.java
@@ -29,8 +29,9 @@ import javax.management.MalformedObjectNameException;
 import javax.management.NotCompliantMBeanException;
 import javax.naming.ConfigurationException;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.collections.MapUtils;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.aop.framework.Advised;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
@@ -286,5 +287,23 @@ public class ComponentContext implements ApplicationContextAware {
 
     private static synchronized void initInitializeBeans(boolean initializeBeans) {
         s_initializeBeans = initializeBeans;
+    }
+
+    public static <T> T getDelegateComponentOfType(Class<T> beanType) {
+        if (s_appContextDelegates == null) {
+            throw new NoSuchBeanDefinitionException(beanType.getName());
+        }
+        T bean = null;
+        for (ApplicationContext context : s_appContextDelegates.values()) {
+            Map<String, T> map = context.getBeansOfType(beanType);
+            if (MapUtils.isNotEmpty(map)) {
+                bean = (T)map.values().toArray()[0];
+                break;
+            }
+        }
+        if (bean == null) {
+            throw new NoSuchBeanDefinitionException(beanType.getName());
+        }
+        return bean;
     }
 }