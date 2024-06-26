7639548f75f83fcc07e4e6f99dad9fef2146a0aa
Vishesh <vishesh92@gmail.com>
2024-06-26 12:09:56 +0530

    Honour listall param for list templates api (#9300)
    * Honour listall param for list templates api
    
    * Don't show templates from another project inside a project view

--- ui/src/views/compute/DeployVM.vue
+++ ui/src/views/compute/DeployVM.vue
@@ -2503,11 +2503,16 @@ export default {
       args.domainid = store.getters.project?.id ? null : this.owner.domainid
       args.projectid = store.getters.project?.id || this.owner.projectid
       args.templatefilter = templateFilter
-      args.projectid = -1
+      if (this.$store.getters.project?.id) {
+        args.projectid = this.$store.getters.project.id
+      } else {
+        args.projectid = -1
+      }
       args.details = 'all'
       args.showicon = 'true'
       args.id = this.templateId
       args.isvnf = false
+      args.listall = true
 
       return new Promise((resolve, reject) => {
         api('listTemplates', args).then((response) => {
@@ -2526,10 +2531,15 @@ export default {
       }
       args.zoneid = _.get(this.zone, 'id')
       args.isoFilter = isoFilter
-      args.projectid = -1
+      if (this.$store.getters.project?.id) {
+        args.projectid = this.$store.getters.project.id
+      } else {
+        args.projectid = -1
+      }
       args.bootable = true
       args.showicon = 'true'
       args.id = this.isoId
+      args.listall = true
 
       return new Promise((resolve, reject) => {
         api('listIsos', args).then((response) => {
