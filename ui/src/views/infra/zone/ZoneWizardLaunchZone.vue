93cbdac60935435c4d6017a19781ae24f95f2413
Wei Zhou <weizhou@apache.org>
2024-06-26 09:59:35 +0200

    UI: ignore error host is already in the database when create a zone (#9231)

--- ui/src/views/infra/zone/ZoneWizardLaunchZone.vue
+++ ui/src/views/infra/zone/ZoneWizardLaunchZone.vue
@@ -2161,7 +2161,11 @@ export default {
           resolve()
         }).catch(error => {
           message = error.response.headers['x-description']
-          reject(message)
+          if (message.includes('is already in the database')) {
+            resolve()
+          } else {
+            reject(message)
+          }
         })
       })
     },
