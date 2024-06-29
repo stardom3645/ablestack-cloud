5abdd03948006e1a9566e331d0f98ac93cfa7d39
Abhisar Sinha <63767682+abh1sar@users.noreply.github.com>
2024-06-29 10:03:34 +0530

    Change storage pool scope from Cluster to Zone and vise versa (#8875)
    * New feature: Change storage pool scope
    
    * Added checks for Ceph/RBD
    
    * Update op_host_capacity table on primary storage scope change
    
    * Storage pool scope change integration test
    
    * pull 8875 : Addressed review comments
    
    * Pull 8875: remove storage checks, AbstractPrimayStorageLifeCycleImpl class
    
    * Pull 8875: Fixed integration test failure
    
    * Pull 8875: Review comments
    
    * Pull 8875: review comments + broke changeStoragePoolScope into smaller functions
    
    * Added UT for changeStoragePoolScope
    
    * Rename AbstractPrimaryDataStoreLifeCycleImpl to BasePrimaryDataStoreLifeCycleImpl
    
    * Pull 8875: Dao review comments
    
    * Pull 8875: Rename changeStoragePoolScope.vue to ChangeStoragePoolScope.vue
    
    * Pull 8875: Created a new smokes test file + A single warning msg in ui
    
    * Pull 8875: Added cleanup in test_primary_storage_scope.py
    
    * Pull 8875: Type in en.json
    
    * Pull 8875: cleanup array in test_primary_storage_scope.py
    
    * Pull:8875 Removing extra whitespace at eof of StorageManagerImplTest
    
    * Pull 8875: Added UT for PrimaryDataStoreHelper and BasePrimaryDataStoreLifeCycleImpl
    
    * Pull 8875: Added license header
    
    * Pull 8875: Fixed sql query for vmstates
    
    * Pull 8875: Changed icon plus info on disabled mode in apidoc
    
    * Pull 8875: Change scope should not work for local storage
    
    * Pull 8875: Change scope completion event
    
    * Pull 8875: Added api findAffectedVmsForStorageScopeChange
    
    * Pull 8875: Added UT for findAffectedVmsForStorageScopeChange and removed listByPoolIdVMStatesNotInCluster
    
    * Pull 8875: Review comments + Vm name in response
    
    * Pull 8875: listByVmsNotInClusterUsingPool was returning duplicate VM entries because of multiple volumes in the VM satisfying the criteria
    
    * Pull 8875: fixed listAffectedVmsForStorageScopeChange UT
    
    * listAffectedVmsForStorageScopeChange should work if the pool is not disabled
    
    * Fix listAffectedVmsForStorageScopeChangeTest UT
    
    * Pull 8875: add volume.removed not null check in VmsNotInClusterUsingPool query
    
    * Pull 8875: minor refactoring in changeStoragePoolScopeToCluster
    
    * Update server/src/main/java/com/cloud/storage/StorageManagerImpl.java
    
    * fix eof
    
    * changeStoragePoolScopeToZone should connect pool to all Up hosts
    
    Co-authored-by: Suresh Kumar Anaparti <sureshkumar.anaparti@gmail.com>

--- ui/src/core/lazy_lib/icons_use.js
+++ ui/src/core/lazy_lib/icons_use.js
@@ -22,6 +22,7 @@ import {
   AppstoreAddOutlined,
   AppstoreOutlined,
   ArrowDownOutlined,
+  ArrowRightOutlined,
   ArrowUpOutlined,
   ArrowsAltOutlined,
   AuditOutlined,
@@ -196,6 +197,7 @@ export default {
     app.component('AppstoreAddOutlined', AppstoreAddOutlined)
     app.component('AppstoreOutlined', AppstoreOutlined)
     app.component('ArrowDownOutlined', ArrowDownOutlined)
+    app.component('ArrowRightOutlined', ArrowRightOutlined)
     app.component('ArrowUpOutlined', ArrowUpOutlined)
     app.component('ArrowsAltOutlined', ArrowsAltOutlined)
     app.component('AuditOutlined', AuditOutlined)
