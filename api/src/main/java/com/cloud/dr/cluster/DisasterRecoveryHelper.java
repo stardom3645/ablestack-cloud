c6bff3125f63834854d8d6f80778d5867d263409
Dajeong-Park <ekwjd7182@gmail.com>
2024-07-18 18:17:16 +0900

    Revert "server와 plugin 의 dependency 상속을 위해 DisasterRecoveryHelper 추가"
    This reverts commit 258a10e0573019d64d3437819bf47eebccd73fc6.

--- api/src/main/java/com/cloud/dr/cluster/DisasterRecoveryHelper.java
+++
@@ -1,9 +0,0 @@
-package com.cloud.dr.cluster;
-
-import com.cloud.utils.component.Adapter;
-
-public interface DisasterRecoveryHelper extends Adapter {
-
-    void checkVmCanBeDestroyed(long vmId);
-
-}
