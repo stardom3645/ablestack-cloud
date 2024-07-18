258a10e0573019d64d3437819bf47eebccd73fc6
Dajeong-Park <ekwjd7182@gmail.com>
2024-07-18 17:29:07 +0900

    server와 plugin 의 dependency 상속을 위해 DisasterRecoveryHelper 추가

---
+++ api/src/main/java/com/cloud/dr/cluster/DisasterRecoveryHelper.java
@@ -0,0 +1,9 @@
+package com.cloud.dr.cluster;
+
+import com.cloud.utils.component.Adapter;
+
+public interface DisasterRecoveryHelper extends Adapter {
+
+    void checkVmCanBeDestroyed(long vmId);
+
+}
