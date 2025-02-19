apache/cloudstack

Primera pure patches & various small fixes (#10132)
Co-authored-by: GLOVER RENE <rg9975@cs419-mgmtserver.rg9975nprd.app.ecp.att.com>
Co-authored-by: Suresh Kumar Anaparti <sureshkumar.anaparti@gmail.com>

framework/spring/lifecycle/src/main/java/org/apache/cloudstack/spring/lifecycle/registry/RegistryLifecycle.java
@@ -108,10 +108,15 @@ public class RegistryLifecycle implements BeanPostProcessor, SmartLifecycle, App
 
         while (iter.hasNext()) {
             Object next = iter.next();
-            if (registry.register(next)) {
-                log.debug("Registered " + next);
-            } else {
-                iter.remove();
+            try {
+                if (registry.register(next)) {
+                    log.debug("Registered " + next);
+                } else {
+                    log.warn("Bean registration failed for " + next.toString());
+                    iter.remove();
+                }
+            } catch (Throwable e) {
+                log.warn("Bean registration attempt resulted in an exception for " + next.toString(), e);
             }
         }
     }

services/secondary-storage/server/src/main/java/org/apache/cloudstack/storage/resource/LocalNfsSecondaryStorageResource.java
@@ -77,14 +77,14 @@ public class LocalNfsSecondaryStorageResource extends NfsSecondaryStorageResourc
 
         // Change permissions for the mountpoint - seems to bypass authentication
         Script script = new Script(true, "chmod", _timeout, s_logger);
-        script.add("777", localRootPath);
+        script.add("1777", localRootPath);
         String result = script.execute();
         if (result != null) {
             String errMsg = "Unable to set permissions for " + localRootPath + " due to " + result;
             s_logger.error(errMsg);
             throw new CloudRuntimeException(errMsg);
         }
-        s_logger.debug("Successfully set 777 permission for " + localRootPath);
+        s_logger.debug("Successfully set 1777 permission for " + localRootPath);
 
         // XXX: Adding the check for creation of snapshots dir here. Might have
         // to move it somewhere more logical later.

stardom3645/ablestack-cloud

framework/spring/lifecycle/src/main/java/org/apache/cloudstack/spring/lifecycle/registry/RegistryLifecycle.java
@@ -109,10 +109,15 @@ public class RegistryLifecycle implements BeanPostProcessor, SmartLifecycle, App
 
         while (iter.hasNext()) {
             Object next = iter.next();
-            if (registry.register(next)) {
-                logger.debug("Registered " + next);
-            } else {
-                iter.remove();
+            try {
+                if (registry.register(next)) {
+                    logger.debug("Registered " + next);
+                } else {
+                    logger.warn("Bean registration failed for " + next.toString());
+                    iter.remove();
+                }
+            } catch (Throwable e) {
+                logger.warn("Bean registration attempt resulted in an exception for " + next.toString(), e);
             }
         }
     }

services/secondary-storage/server/src/main/java/org/apache/cloudstack/storage/resource/LocalNfsSecondaryStorageResource.java
@@ -75,14 +75,14 @@ public class LocalNfsSecondaryStorageResource extends NfsSecondaryStorageResourc
 
         // Change permissions for the mountpoint - seems to bypass authentication
         Script script = new Script(true, "chmod", _timeout, logger);
-        script.add("777", localRootPath);
+        script.add("1777", localRootPath);
         String result = script.execute();
         if (result != null) {
             String errMsg = "Unable to set permissions for " + localRootPath + " due to " + result;
             logger.error(errMsg);
             throw new CloudRuntimeException(errMsg);
         }
-        logger.debug("Successfully set 777 permission for " + localRootPath);
+        logger.debug("Successfully set 1777 permission for " + localRootPath);
 
         // XXX: Adding the check for creation of snapshots dir here. Might have
         // to move it somewhere more logical later.