5e44c34147df40c5e16087ef81e39c79b02877bf
dahn <daan@onecht.net>
2025-02-20 09:01:28 +0100

    ipmi: extra log sanitation (#10428)

--- plugins/outofbandmanagement-drivers/ipmitool/src/main/java/org/apache/cloudstack/outofbandmanagement/driver/ipmitool/IpmitoolWrapper.java
+++ plugins/outofbandmanagement-drivers/ipmitool/src/main/java/org/apache/cloudstack/outofbandmanagement/driver/ipmitool/IpmitoolWrapper.java
@@ -27,6 +27,7 @@ import org.apache.cloudstack.utils.process.ProcessResult;
 import org.apache.cloudstack.utils.process.ProcessRunner;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
 import org.joda.time.Duration;
 
 import java.util.ArrayList;
@@ -157,25 +158,32 @@ public final class IpmitoolWrapper {
     public OutOfBandManagementDriverResponse executeCommands(final List<String> commands, final Duration timeOut) {
         final ProcessResult result = RUNNER.executeCommands(commands, timeOut);
         if (logger.isTraceEnabled()) {
-            List<String> cleanedCommands = new ArrayList<String>();
-            int maskNextCommand = 0;
-            for (String command : commands) {
-                if (maskNextCommand > 0) {
-                    cleanedCommands.add("**** ");
-                    maskNextCommand--;
-                    continue;
-                }
-                if (command.equalsIgnoreCase("-P")) {
-                    maskNextCommand = 1;
-                } else if (command.toLowerCase().endsWith("password")) {
-                    maskNextCommand = 2;
-                }
-                cleanedCommands.add(command);
-            }
-            logger.trace("Executed ipmitool process with commands: " + StringUtils.join(cleanedCommands, ", ") +
-                      "\nIpmitool execution standard output: " + result.getStdOutput() +
-                      "\nIpmitool execution error output: " + result.getStdError());
+            List<String> cleanedCommands = getSanatisedCommandStrings(commands);
+            logger.trace("Executed ipmitool process with commands: {}\nIpmitool execution standard output: {}\nIpmitool execution error output: {}",
+                    StringUtils.join(cleanedCommands, ", "),
+                    result.getStdOutput(),
+                    result.getStdError());
         }
         return new OutOfBandManagementDriverResponse(result.getStdOutput(), result.getStdError(), result.isSuccess());
+    }
+
+    @NotNull
+    List<String> getSanatisedCommandStrings(List<String> commands) {
+        List<String> cleanedCommands = new ArrayList<String>();
+        int maskNextCommand = 0;
+        for (String command : commands) {
+            if (maskNextCommand > 0) {
+                cleanedCommands.add("**** ");
+                maskNextCommand--;
+                continue;
+            }
+            if (command.equalsIgnoreCase("-P")) {
+                maskNextCommand = 1;
+            } else if (command.toLowerCase().endsWith("password")) {
+                maskNextCommand = 2;
+            }
+            cleanedCommands.add(command);
+        }
+        return cleanedCommands;
     }
 }
