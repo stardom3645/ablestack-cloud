720b20027bb1fe32c9e05b2329dd18fb01a27ef0
dahn <daan@onecht.net>
2025-04-14 10:17:56 +0200

    undo removal of accessLogger and deal with some warnings (#10567)
    * undo removval of accessLogger and deal with some warnings
    
    * Apply suggestions from code review
    
    * add accesslog to servlet

--- server/src/main/java/com/cloud/api/ApiServlet.java
+++ server/src/main/java/com/cloud/api/ApiServlet.java
@@ -75,9 +75,7 @@ import com.cloud.utils.net.NetUtils;
 @Component("apiServlet")
 public class ApiServlet extends HttpServlet {
     protected static Logger LOGGER = LogManager.getLogger(ApiServlet.class);
-    private final static List<String> s_clientAddressHeaders = Collections
-            .unmodifiableList(Arrays.asList("X-Forwarded-For",
-                    "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR", "Remote_Addr"));
+    private static final Logger ACCESSLOGGER = LogManager.getLogger("apiserver." + ApiServlet.class.getName());
     private static final String REPLACEMENT = "_";
     private static final String LOGGER_REPLACEMENTS = "[\n\r\t]";
 
@@ -373,7 +371,7 @@ public class ApiServlet extends HttpServlet {
             LOGGER.error("unknown exception writing api response", ex);
             auditTrailSb.append(" unknown exception writing api response");
         } finally {
-            LOGGER.debug(auditTrailSb.toString());
+            ACCESSLOGGER.info(auditTrailSb.toString());
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("===END=== " + reqStr);
             }
