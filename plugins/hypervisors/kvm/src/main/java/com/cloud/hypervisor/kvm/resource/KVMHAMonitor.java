93d967ae06084e9bb0a406d749c1dc5097f8b89d
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2025-04-14 17:08:17 +0900

    CLVM HA기능 테스트 1

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/KVMHAMonitor.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/KVMHAMonitor.java
@@ -243,11 +243,10 @@ public class KVMHAMonitor extends KVMHABase implements Runnable {
     @Override
     public void run() {
         while (true) {
-
-            logger.info(String.format("### HA Checking for NFS Type Storage Pools : %s", storagePool));
-            logger.info(String.format("### HA Checking for SharedMountPoint Type Storage Pools : %s", storageGfsPool));
-            logger.info(String.format("### HA Checking for RBD Type Storage Pools : %s", storageRbdPool));
-            logger.info(String.format("### HA Checking for CLVN Type Storage Pools : %s", storageClvmPool));
+            logger.info(String.format("### [HA Checking] for NFS Type Storage Pools : %s", storagePool));
+            logger.info(String.format("### [HA Checking] for SharedMountPoint Type Storage Pools : %s", storageGfsPool));
+            logger.info(String.format("### [HA Checking] for RBD Type Storage Pools : %s", storageRbdPool));
+            logger.info(String.format("### [HA Checking] for CLVM Type Storage Pools : %s", storageClvmPool));
 
             runHeartBeat(storagePool);
             runHeartBeat(storageGfsPool);
