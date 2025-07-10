2b0440d7d72caa6a7e0ce2e35335ea4f4790cac5
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2025-07-10 12:33:48 +0900

    병합 소스 수정 10

--- ui/src/config/section/infra/hosts.js
+++ ui/src/config/section/infra/hosts.js
@@ -33,7 +33,7 @@ export default {
   params: { type: 'routing' },
   columns: () => {
     const fields = [
-      'name', 'state', 'resourcestate', 'ipaddress', 'arch', 'hypervisor', 
+      'name', 'state', 'resourcestate', 'ipaddress', 'arch', 'hypervisor',
       { field: 'systeminstances', customTitle: 'system.vms' }, 'version', 'haenable', 'hastate']
     const metricsFields = ['instances', 'powerstate', 'cpunumber', 'cputotalghz', 'cpuusedghz', 'cpuallocatedghz', 'memorytotalgb', 'memoryusedgb', 'memoryallocatedgb', 'networkread', 'networkwrite']
     if (store.getters.metrics) {
