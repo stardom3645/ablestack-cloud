2a6763db2a92b314f85ea98eb3c365c0e93dfa96
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2025-09-11 15:54:04 +0900

    extraconfig 사용중인 가상머신이 라이브마이그레이션시 안내메시지 추가

--- ui/src/config/section/compute.js
+++ ui/src/config/section/compute.js
@@ -360,9 +360,9 @@ export default {
           show: (record, store) => {
             return ['Running'].includes(record.state) && ['Admin'].includes(store.userInfo.roletype) && !record.kvdoinuse
           },
-          disabled: (record) => {
-            return record.details && 'extraconfig-1' in record.details
-          },
+          // disabled: (record) => {
+          //   return record.details && 'extraconfig-1' in record.details
+          // },
           tooltip: (record) => {
             if (record.details && 'extraconfig-1' in record.details) {
               return 'label.enable.host'
